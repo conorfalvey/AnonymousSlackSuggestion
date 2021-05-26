074c22d4a3602880f983bfdae88c746ae8ebd8fa
Conor <conor.falvey22@gmail.com>
2021-05-26 10:47:46 -0700

    Enhance environment variable handling

--- bot.py
+++ bot.py
@@ -17,9 +17,24 @@ from flask import Flask, request, Response
 import slack
 from slackeventsapi import SlackEventAdapter
 
-# Load environment variables
+# Load environment variables and verify
 env_path = Path(".") / ".env"
-load_dotenv(dotenv_path=env_path)
+if os.path.exists(env_path):
+    load_dotenv(dotenv_path=env_path)
+
+required_keys = [
+    "SIGNING_SECRET",
+    "SLACK_TOKEN",
+    "SLACK_HEADER",
+    "FLASK_DEBUG",
+    "FLASK_PORT",
+]
+either_keys = ["SLACK_MEMBER_ID", "SLACK_RECIPIENT"]
+
+if not all([key in os.environ for key in required_keys]) or not any(
+    [key in os.environ for key in either_keys]
+):
+    raise EnvironmentError("Not all required environment variables are set!")
 
 # Server configs and API secrets
 app = Flask(__name__)
@@ -31,7 +46,7 @@ slackClient = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 BOT_ID = slackClient.api_call("auth.test")["user_id"]
 
 
-@app.route("/anonSuggest", methods=["POST"])
+@app.route("/matt", methods=["POST"])
 def anonSuggest():
     """ Recieve POST requests from Slack API
     served by the `/suggest` command."""
@@ -40,14 +55,16 @@ def anonSuggest():
 
     # retrieve_recipient looks up with user_id from the provided
     # recipient username in the .env file. It queries all members
-    # and matches on the username.
-    recipient = retrieve_recipient()
-    if not recipient:
+    # and matches on the username if the member ID is not given.
+    if not os.environ["SLACK_MEMBER_ID"]:
+        os.environ["SLACK_MEMBER_ID"] = retrieve_recipient()
+
+    if not os.environ["SLACK_MEMBER_ID"]:
         return Response(), 500
 
     # send_suggestion appends a message prefix defined in the
     # .env file. They're just concatenated.
-    send_suggestion(slackClient, recipient=recipient, text=text)
+    send_suggestion(slackClient, recipient=os.environ["SLACK_MEMBER_ID"], text=text)
     return Response(), 200
 
 
@@ -60,6 +77,7 @@ def retrieve_recipient():
 
 def send_suggestion(slackClient, recipient, text):
     updated_text = os.environ["SLACK_HEADER"] + text
+    # Channel arg can be a community channel or a single user ID.
     slackClient.chat_postMessage(channel=recipient, text=updated_text)
 
 
